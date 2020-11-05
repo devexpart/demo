b6e12e7b17361ec88888df896bad5ea93cc581e5
Faisal <faisal.qayyum@outlook.com>
2020-11-05 23:19:20 +0500

    My first GIT app
    Signed-off-by: Faisal <faisal.qayyum@outlook.com>

---
+++ DemoGitApp/DemoGitApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoGitApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
